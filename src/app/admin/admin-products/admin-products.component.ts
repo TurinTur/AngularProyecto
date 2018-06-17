1f12ec4e3d9e36921bb5564aa0deb2b5fe39df7b
Turin <josedavidmoreno@hotmail.com>
2018-06-17 23:46:57 +0200

    316 redirecting users bug

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -4,6 +4,7 @@ import { map } from 'rxjs/operators';
 import { Observable, Subscription } from 'rxjs';
 import { AngularFireAction, DatabaseSnapshot } from 'angularfire2/database';
 import { Product, ProductKey } from '../../models/product';
+import { DataTableResource } from '@mindsorg/ng-data-table';
 
 @Component({
   selector: 'app-admin-products',
@@ -14,9 +15,11 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
 
   //products$: any;   // Con el sistema de filtrado, tengo que tener los productos en un array, asi que dejo de usar un observable
   products: ProductKey[];   
-  filteredProducts: ProductKey[];
   productsKeys$: any;
   subscription: Subscription;
+  tableResource: DataTableResource<ProductKey>;
+  items: ProductKey[] = [];
+  itemCount: number;
 
   constructor(private ProductService: ProductService) { 
     //this.products$ = this.ProductService.getAll();              // forma original, no tiene keys
@@ -24,10 +27,33 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
     //this.subscription = this.ProductService.getAll().subscribe(p =>  this.filteredProducts = this.products = p);   // forma nueva, no me vale porque pierdo las keys que tenia en productsKeys, yo no tengo p.$key
     
     this.subscription =this.productsKeys$ = this.ObtenerKeys().subscribe(p => {
-       this.filteredProducts= this.products = p;
+       this.products = p;
+       this.initializeTable(this.products);
+
     });          
     
     
+  }
+
+  private initializeTable (products: ProductKey[]){
+    this.tableResource = new DataTableResource(products);
+    this.tableResource.query({ offset:0})  // obtiene los datos según un offset. offset = 0 para la primera pagina, 50 para la 2ª si queremos 50 por pagina, etc
+     .then( items => {
+       this.items= items;
+    });
+   this.tableResource.count()
+     .then(count => { 
+       this.itemCount = count;
+    });
+  }
+
+  reloadItems (params){
+
+    if (!this.tableResource) return;
+
+    console.log(params)
+    this.tableResource.query(params)  
+    .then( items => this.items= items);
   }
 
   ngOnInit() {
@@ -37,7 +63,7 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  ObtenerKeys () : Observable<ProductKey[]>{        // He entido que refactorizar para que devuelvas los tipos especificos, ya que si no, no podia asignarlos a mis props.
+  ObtenerKeys () : Observable<ProductKey[]>{        // He tenido que refactorizar para que devuelvas los tipos especificos, ya que si no, no podia asignarlos a mis props.
     return this.ProductService.getAllSnapshot()
      .pipe(map(items => {            
        return items.map(a => {
@@ -54,7 +80,9 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
 
   filter(query: string){
     console.log(query);
-    this.filteredProducts = (query) ?   // Si hay query, aplicar el filtro
+    let filteredProducts = (query) ?   // Si hay query, aplicar el filtro
       this.products.filter(p => p.data.title.toLowerCase().includes(query.toLowerCase())) : this.products; // si no, coger el array inicial
+    
+    this.initializeTable(filteredProducts);
   }
 }
