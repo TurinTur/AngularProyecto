c5a05683282d1e43a210eaad664988de9e02ef42
Turin <josedavidmoreno@hotmail.com>
2018-07-03 21:06:10 +0200

    365 modules

--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -1,38 +1,26 @@
 import { OrderService } from './../order.service';
+import { Component } from '@angular/core';
 import { AuthService } from './../auth.service';
-import { Component, OnInit } from '@angular/core';
-import { switchMap, map } from 'rxjs/operators';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-my-orders',
   templateUrl: './my-orders.component.html',
   styleUrls: ['./my-orders.component.css']
 })
-export class MyOrdersComponent implements OnInit {
+export class MyOrdersComponent  {
 
-  orders$;
+  orders$: Observable<{}[]>;
 
-  constructor(private authService: AuthService, private orderService: OrderService,private db: AngularFireDatabase,) { 
+  constructor(private authService: AuthService, private orderService: OrderService) { 
      this.orders$ = authService.user$.pipe(switchMap(u =>            // Switch, primero consigo el usuario, y luego uso el usario para obtener la orden
-        this.db.list('/orders', ref => 
-          u ? ref.orderByChild('userId').equalTo(u.uid) : ref ).valueChanges() 
-        )
-        //orderService.getOrdersByUser(u.uid))   
+      
+     orderService.getOrdersByUser(u.uid) )   
+ 
     );
 
   }
 
-  async ngOnInit() {
-    /* this.authService.user$.subscribe(async x => {
-      console.log(x.uid);
-      this.orders$= await this.orderService.getOrdersByUser(x.uid);
  
-    for (const iterator of this.orders$) {
-      console.log(iterator);
-    }
-    }); */
-   
-  }
-
 }
--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,15 +1,16 @@
 import { ShoppingCartService } from './shopping-cart.service';
-import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
-import { Query } from '@firebase/database';
-
+import { AuthService } from './auth.service';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor(private db: AngularFireDatabase, private shoppingCartService: ShoppingCartService) { }
+  constructor(private authService: AuthService,private db: AngularFireDatabase, private shoppingCartService: ShoppingCartService) { }
 
   async placeOrder(order){
     let result = await this.db.list('/orders').push(order); //Estas dos lineas se deberían hacer en una transacción, y si una de las dos falla, ninguna de las dos debería ejecutarse.
@@ -18,12 +19,13 @@ export class OrderService {
     return result;
   }
 
-  getOrders () {
+  getOrders (): Observable<{}[]> {
     return this.db.list('/orders').valueChanges();
   }
 
-*  getOrdersByUser (userId: string){
-    return this.db.list('/orders', ref => ref.orderByChild('userId').equalTo(userId)).valueChanges();
+ getOrdersByUser (userId: string) {// : Observable<{}[]>{
+      return this.db.list('/orders', ref => ref.orderByChild('userId').equalTo(userId)).valueChanges()
+      
   } 
 
 
